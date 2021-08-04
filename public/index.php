04efadb13e0e3f30495a11394065c7b0177fa776
Daniel Podrabinek <dpodrabinek@oktopost.com>
2021-08-04 13:20:12 +0300

    Standards fix

--- public/index.php
+++ public/index.php
@@ -2,9 +2,16 @@
 
 if (isset($_GET['with_redirect']))
 {
+	$code = isset($_GET['code']) ? $_GET['code'] : 302;
+	
 	if (!isset($_GET['is_redirected']))
 	{
-		header('Location: index.php?with_redirect=true&is_redirected='.mt_rand());
+		header('Location: index.php?with_redirect=true&is_redirected=' . mt_rand() , true, $code);
+		exit();
+	}
+	else if (isset($_GET['redirect_to']))
+	{
+		header('Location: ' . $_GET['redirect_to'] . true, $code);
 		exit();
 	}
 }
@@ -23,26 +30,26 @@ $author = mb_strtolower($faker->lastName);
 	<meta charset="utf-8">
 	<title><?= $title ?></title>
 	<meta name="viewport" content="width=device-width,initial-scale=1.0">
-	
+
 	<meta name="image" property="og:image" content="<?= $image ?>">
 	<meta name="title" property="og:title" content="<?= $title ?>">
-	
+
 	<meta name="twitter:card" content="summary_large_image">
 	<meta name="twitter:title" content="<?= $title ?>">
 	<meta name="twitter:image" content="<?= $image ?>">
 	<meta name="twitter:site" content="@<?= $author ?>">
 	<meta name="twitter:creator" content="@<?= $author ?>">
-	
+
 	<meta name="description" property="og:description" content="<?= $description ?>">
 	<meta name="twitter:description" content="<?= $description ?>">
-	
+
 	<link rel="icon" href="data:;base64,iVBORw0KGgo=">
 </head>
 <body>
 <div id="app">
 	<h1><?= $title ?></h1>
 	<img src="<?= $image ?>"
-		 
+
 	<p><?= $description ?></p>
 </div>
 </body>
